55e36d34608232c39a60f87577712f5c2e149f91
Tadd Giles <tadd@taddgiles.com>
2016-05-23 16:20:09 -0600

    Update

--- cmd/platform-test-go/main.go
+++ cmd/platform-test-go/main.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/auth0/go-jwt-middleware"
-  "github.com/dgrijalva/jwt-go"
-  "github.com/gorilla/context"
+  "os"
+  "github.com/gin-gonic/gin"
 )
 
-var myHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-  user := context.Get(r, "user")
-  fmt.Fprintf(w, "This is an authenticated request")
-  fmt.Fprintf(w, "Claim content:\n")
-  for k, v := range user.(*jwt.Token).Claims {
-    fmt.Fprintf(w, "%s :\t%#v\n", k, v)
-  }
-})
-
 func main() {
-  jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
-    ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-      return []byte("secret"), nil
-    },
-    // When set, the middleware verifies that tokens are signed with the specific signing algorithm
-    // If the signing method is not constant the ValidationKeyGetter callback can be used to implement additional checks
-    // Important to avoid security issues described here: https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/
-    SigningMethod: jwt.SigningMethodHS256,
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "3000"
+  }
+
+
+  r := gin.New()
+  r.Use(gin.Logger())
+
+  r.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "ok",
+    })
   })
 
-  app := jwtMiddleware.Handler(myHandler)
-  http.ListenAndServe("0.0.0.0:3000", app)
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+
+  r.Run(":" + port)
 }
