0c22d1b3932b8f8c367517eb8ef879ad03a435dd
Tadd Giles <tadd@taddgiles.com>
2016-05-23 17:06:35 -0600

    update

--- cmd/platform-test-go/main.go
+++ cmd/platform-test-go/main.go
@@ -2,30 +2,78 @@ package main
 
 import (
   "os"
+  // "log"
+
+  "github.com/taddgiles/platform-test-go/db"
   "github.com/gin-gonic/gin"
 )
 
+const (
+  Port = "3000"
+)
+
+type User struct {
+  Name string
+  Email string
+}
+
+func init() {
+  db.Connect()
+}
+
 func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "3000"
+  jwtsecret := os.Getenv("JWT_SECRET")
+  if jwtsecret == "" {
+    jwtsecret = "secret"
   }
 
+  // db := os.Getenv("MONGODB_URI")
+  // if db == "" {
+  //   db = "localhost"
+  // }
 
-  r := gin.New()
-  r.Use(gin.Logger())
+  // session, err := mgo.Dial(db)
+  // if err != nil {
+  //   panic(err)
+  // }
+  // defer session.Close()
 
-  r.GET("/", func(c *gin.Context) {
+  // users := session.DB("platform-test").C("users")
+  // err = c.Insert(&User{"Ale", "whatnow@email.com"},
+  //          &User{"Cla", "whatnow2@email.com"})
+  // if err != nil {
+  //   log.Fatal(err)
+  // }
+
+  // result := User{}
+
+  router := gin.Default()
+
+  router.GET("/", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "ok",
     })
   })
 
-  r.GET("/ping", func(c *gin.Context) {
+  router.POST("/authenticate", func(c *gin.Context) {
+    // email := c.Query("email")
+    // password := c.Query("password")
+
+    // err = users.Find(bson.M{"email": String(email)}).One(&result)
+    // if err != nil {
+    //   log.Fatal(err)
+    // }
+    // log.Printf("Email:", result)
+
     c.JSON(200, gin.H{
-      "message": "pong",
+      "message": "ok",
     })
   })
 
-  r.Run(":" + port)
+// Start listening
+  port := Port
+  if len(os.Getenv("PORT")) > 0 {
+    port = os.Getenv("PORT")
+  }
+  router.Run(":" + port)
 }
